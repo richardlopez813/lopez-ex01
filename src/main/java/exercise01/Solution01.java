a8c8c6ab6063c9700cbdf1686f519bb2388d033b
richard lopez <richardlopez813>
2021-09-05 21:57:55 -0400

    final

--- src/main/java/exercise01/Solution01.java
+++ src/main/java/exercise01/Solution01.java
@@ -1,3 +1,15 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Richard Lopez
+ */
+
+/*
+    1. Input Scanner
+    2. prompt user and ask them what their name is
+    3. Scan for name
+    4. Output statement with name given
+ */
+
 package exercise01;
 
 import java.util.Scanner;
@@ -5,16 +17,10 @@ import java.util.Scanner;
 public class Solution01 {
     private static final Scanner in = new Scanner(System.in);
 
-    private static void main(String[] args){
+    public static void main(String[] args){
         System.out.print("What is your name? ");
         String name = in.nextLine();
-        System.out.printf("Hello, %s, nice to meet you", name);
-
-        /*
-        1. Input Scanner
-        2. prompt user and ask them what their name is
-        3. Scan for name
-        4. Create formatted print using the result of the input.
-         */
+        String statement = "Hello " + name + " nice to meet you!";
+        System.out.print(statement);
     }
 }
